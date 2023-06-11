ba331d2311a57c86572add15c597a121fd876e08
Keith Gaputis <keith.gaputis@snowflake.com>
2023-06-11 16:23:11 -0400

    Modify conda env name, formatting changes

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -1,14 +1,15 @@
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Hands-On Lab: Data Engineering with Snowpark
 # Script:       02_load_raw.py
 # Author:       Jeremiah Hansen, Caleb Baechtold
 # Last Updated: 1/9/2023
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 import time
 from snowflake.snowpark import Session
-#import snowflake.snowpark.types as T
-#import snowflake.snowpark.functions as F
+
+# import snowflake.snowpark.types as T
+# import snowflake.snowpark.functions as F
 
 
 POS_TABLES = ['country', 'franchise', 'location', 'menu', 'truck', 'order_header', 'order_detail']
@@ -17,6 +18,7 @@ TABLE_DICT = {
     "pos": {"schema": "RAW_POS", "tables": POS_TABLES},
     "customer": {"schema": "RAW_CUSTOMER", "tables": CUSTOMER_TABLES}
 }
+
 
 # SNOWFLAKE ADVANTAGE: Schema detection
 # SNOWFLAKE ADVANTAGE: Data ingestion with COPY
@@ -27,13 +29,14 @@ def load_raw_table(session, tname=None, s3dir=None, year=None, schema=None):
     if year is None:
         location = "@external.frostbyte_raw_stage/{}/{}".format(s3dir, tname)
     else:
-        print('\tLoading year {}'.format(year)) 
+        print('\tLoading year {}'.format(year))
         location = "@external.frostbyte_raw_stage/{}/{}/year={}".format(s3dir, tname, year)
-    
+
     # we can infer schema using the parquet read option
     df = session.read.option("compression", "snappy") \
-                            .parquet(location)
+        .parquet(location)
     df.copy_into_table("{}".format(tname))
+
 
 # SNOWFLAKE ADVANTAGE: Warehouse elasticity (dynamic scaling)
 
@@ -55,6 +58,7 @@ def load_all_raw_tables(session):
 
     _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XSMALL").collect()
 
+
 def validate_raw_tables(session):
     # check column names from the inferred schema
     for tname in POS_TABLES:
@@ -68,14 +72,16 @@ def validate_raw_tables(session):
 if __name__ == "__main__":
     # Add the utils package to our path and import the snowpark_utils function
     import os, sys
+
     current_dir = os.getcwd()
     parent_dir = os.path.dirname(current_dir)
     sys.path.append(parent_dir)
 
     from utils import snowpark_utils
+
     session = snowpark_utils.get_snowpark_session()
 
     load_all_raw_tables(session)
-#    validate_raw_tables(session)
+    #    validate_raw_tables(session)
 
     session.close()
